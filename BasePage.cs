31d04714936231798d3a9d3cf5102135d23f879d
msmaneesha <69891335+msmaneesha@users.noreply.github.com>
2021-04-26 19:40:05 +0530

    Add files via upload

---
+++ BasePage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dRd
+{
+    public partial class BasePage : Form
+    {
+        public BasePage()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            T_1_FundusUpload obj = new T_1_FundusUpload();
+            
+            obj.Show();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            D_1_FundusUpload obj = new D_1_FundusUpload();
+            
+            obj.Show();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dRd
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="admin" && textBox2.Text=="pwd")
+            {
+                BasePage obj = new BasePage();
+                ActiveForm.Hide();
+                obj.Show();
+            }
+            else
+            {
+                MessageBox.Show("Login Failed");
+            }
+        }
+    }
+}
