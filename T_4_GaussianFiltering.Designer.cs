e7e65c3ff03e3265276c3e972ed31e52bef95ec0
msmaneesha <69891335+msmaneesha@users.noreply.github.com>
2021-05-19 18:46:14 +0530

    Add files via upload

---
+++ T_4_GaussianFiltering.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace dRd
+{
+    partial class T_4_GaussianFiltering
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.sizeTrackBar = new System.Windows.Forms.TrackBar();
+            this.sizeBox = new System.Windows.Forms.TextBox();
+            this.sigmaBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sigmaTrackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sigmaTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(233, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 24);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "Processed Image";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox2);
+            this.groupBox3.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.groupBox3.ForeColor = System.Drawing.Color.White;
+            this.groupBox3.Location = new System.Drawing.Point(21, 110);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(203, 217);
+            this.groupBox3.TabIndex = 38;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Preview";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 25);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(197, 189);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(234, 135);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(341, 323);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(129)))), ((int)(((byte)(158)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(361, 473);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 39);
+            this.button2.TabIndex = 39;
+            this.button2.Text = "Apply Filter";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // sizeTrackBar
+            // 
+            this.sizeTrackBar.LargeChange = 1;
+            this.sizeTrackBar.Location = new System.Drawing.Point(32, 467);
+            this.sizeTrackBar.Maximum = 9;
+            this.sizeTrackBar.Name = "sizeTrackBar";
+            this.sizeTrackBar.Size = new System.Drawing.Size(193, 45);
+            this.sizeTrackBar.TabIndex = 37;
+            this.sizeTrackBar.Value = 1;
+            this.sizeTrackBar.ValueChanged += new System.EventHandler(this.sizeTrackBar_ValueChanged);
+            // 
+            // sizeBox
+            // 
+            this.sizeBox.Location = new System.Drawing.Point(99, 438);
+            this.sizeBox.Name = "sizeBox";
+            this.sizeBox.Size = new System.Drawing.Size(60, 20);
+            this.sizeBox.TabIndex = 36;
+            this.sizeBox.TextChanged += new System.EventHandler(this.sizeBox_TextChanged);
+            // 
+            // sigmaBox
+            // 
+            this.sigmaBox.Location = new System.Drawing.Point(99, 357);
+            this.sigmaBox.Name = "sigmaBox";
+            this.sigmaBox.Size = new System.Drawing.Size(60, 20);
+            this.sigmaBox.TabIndex = 33;
+            this.sigmaBox.TextChanged += new System.EventHandler(this.sigmaBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(32, 434);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 23);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Size:";
+            // 
+            // sigmaTrackBar
+            // 
+            this.sigmaTrackBar.Location = new System.Drawing.Point(32, 387);
+            this.sigmaTrackBar.Maximum = 50;
+            this.sigmaTrackBar.Name = "sigmaTrackBar";
+            this.sigmaTrackBar.Size = new System.Drawing.Size(196, 45);
+            this.sigmaTrackBar.TabIndex = 34;
+            this.sigmaTrackBar.TickFrequency = 2;
+            this.sigmaTrackBar.ValueChanged += new System.EventHandler(this.sigmaTrackBar_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(32, 360);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 24);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Sigma:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(129)))), ((int)(((byte)(158)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(471, 473);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 39);
+            this.button1.TabIndex = 41;
+            this.button1.Text = "Proceed";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.label3.ForeColor = System.Drawing.Color.Silver;
+            this.label3.Location = new System.Drawing.Point(396, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 25);
+            this.label3.TabIndex = 131;
+            this.label3.Text = "Smoothening of Image";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI Light", 41.25F);
+            this.label11.ForeColor = System.Drawing.Color.Cyan;
+            this.label11.Location = new System.Drawing.Point(194, -6);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(430, 74);
+            this.label11.TabIndex = 130;
+            this.label11.Text = "Gaussian Filtering";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.label5.ForeColor = System.Drawing.Color.Silver;
+            this.label5.Location = new System.Drawing.Point(4, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 25);
+            this.label5.TabIndex = 132;
+            this.label5.Text = "Image Preprocessing...";
+            // 
+            // T_4_GaussianFiltering
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(617, 525);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.sizeTrackBar);
+            this.Controls.Add(this.sizeBox);
+            this.Controls.Add(this.sigmaBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sigmaTrackBar);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "T_4_GaussianFiltering";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T_4_GaussianFiltering";
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sigmaTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar sizeTrackBar;
+        private System.Windows.Forms.TextBox sizeBox;
+        private System.Windows.Forms.TextBox sigmaBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar sigmaTrackBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ T_4_GaussianFiltering.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Math;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Imaging.Textures;
+using AForge;
+
+
+namespace dRd
+{
+    public partial class T_4_GaussianFiltering : Form
+    {
+        public T_4_GaussianFiltering()
+        {
+            InitializeComponent();
+        }
+        private GaussianBlur filter = new GaussianBlur();
+        public static Bitmap bm1;
+        public T_4_GaussianFiltering(Bitmap bmp)
+        {
+            InitializeComponent();
+            bm1 = bmp;
+            sigmaBox.Text = filter.Sigma.ToString();
+            sizeBox.Text = filter.Size.ToString();
+            pictureBox2.Image = (System.Drawing.Image)bmp;
+            sigmaTrackBar.Value = (int)((filter.Sigma - 0.5) * 20);
+            sizeTrackBar.Value = (filter.Size - 3) / 2;
+        }
+
+        private void sigmaTrackBar_ValueChanged(object sender, EventArgs e)
+        {
+            double v = (double)sigmaTrackBar.Value / 20 + 0.5;
+
+            sigmaBox.Text = v.ToString();
+        }
+
+        private void sizeTrackBar_ValueChanged(object sender, EventArgs e)
+        {
+            int v = sizeTrackBar.Value * 2 + 3;
+
+            sizeBox.Text = v.ToString();
+        }
+
+        private void sigmaBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                filter.Sigma = double.Parse(sigmaBox.Text);
+                Bitmap newImage = filter.Apply((Bitmap)bm1);
+                pictureBox2.Image = newImage;
+             
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void sizeBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                filter.Size = int.Parse(sizeBox.Text);
+                Bitmap newImage = filter.Apply((Bitmap)bm1);
+                pictureBox2.Image = newImage;
+              
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap newImage = filter.Apply((Bitmap)bm1);
+            pictureBox1.Image = newImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            T_5_DataAugmentation_Hflip obj = new T_5_DataAugmentation_Hflip();
+            Program.gaussian = (Bitmap)pictureBox1.Image;
+            ActiveForm.Hide();
+            obj.Show();
+        }
+
+
+    }
+}
