21b2f6a4e3d4e5bcd8803e0950bb36ea9b0c5708
msmaneesha <69891335+msmaneesha@users.noreply.github.com>
2021-06-24 13:36:18 +0530

    Add files via upload

---
+++ D_2_Detection.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace dRd
+{
+    partial class D_2_Detection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nodes = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(37, 89);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 234);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 40;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Light", 11.25F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(33, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "Fundus Image";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(129)))), ((int)(((byte)(158)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Light", 11.25F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(35, 329);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 39);
+            this.button1.TabIndex = 41;
+            this.button1.Text = "Load Blocks";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nodes
+            // 
+            this.nodes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(36)))));
+            this.nodes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nodes.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nodes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(129)))), ((int)(((byte)(158)))));
+            this.nodes.FormattingEnabled = true;
+            this.nodes.ItemHeight = 15;
+            this.nodes.Location = new System.Drawing.Point(325, 91);
+            this.nodes.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.nodes.Name = "nodes";
+            this.nodes.Size = new System.Drawing.Size(553, 420);
+            this.nodes.TabIndex = 42;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(129)))), ((int)(((byte)(158)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Light", 11.25F);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(36, 374);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(257, 39);
+            this.button2.TabIndex = 43;
+            this.button2.Text = "Search Blocks";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 11.25F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(321, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "Nodes";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI Light", 31.25F);
+            this.label11.ForeColor = System.Drawing.Color.Cyan;
+            this.label11.Location = new System.Drawing.Point(543, -6);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(324, 57);
+            this.label11.TabIndex = 142;
+            this.label11.Text = "Searching Nodes";
+            // 
+            // D_2_Detection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(900, 525);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.nodes);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "D_2_Detection";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "D_2_Detection";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox nodes;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label11;
+    }
+}
---
+++ D_2_Detection.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace dRd
+{
+    public partial class D_2_Detection : Form
+    {
+        string result = "";
+        public D_2_Detection()
+        {
+            InitializeComponent();
+            pictureBox2.Image = (Image)Image.FromFile(Program.dfunduspath);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             string path =Application.StartupPath+"\\cnn\\cnn.txt";
+             double ss = DateTime.Now.Millisecond;
+             Program.data1 = ss.ToString();
+
+             if (File.Exists(path))
+             {
+                 string text = File.ReadAllText(path);
+                 var chunks1 = Regex.Matches(text, @"(\b.{1,250})")
+              .Cast<Match>().Select(p => p.Groups[1].Value)
+              .ToList();
+                 for (int i = 0; i < chunks1.Count(); i++)
+                 {
+                     nodes.Items.Add(chunks1[i]);
+                 }
+             }
+             double ss1 = nodes.Items.Count + Convert.ToDouble(Program.data1);
+             MessageBox.Show("Total:"+ ss1.ToString() + " nodes Available for search");
+         
+        }
+
+        private static void SetupPyEnv()
+        {
+            string envPythonHome = @"C:\Users\arnal\AppData\Local\Programs\Python\Python37\";
+            string envPythonLib = envPythonHome + "Lib\\;" + envPythonHome + @"Lib\site-packages\";
+            Environment.SetEnvironmentVariable("PYTHONHOME", envPythonHome, EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("PATH", envPythonHome + ";" + envPythonLib + ";" + Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine), EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("PYTHONPATH", envPythonLib, EnvironmentVariableTarget.User);
+
+        }
+        public static Random rnd;
+        public int nInput;
+        public int[] nHidden;
+         int nOutput=0;
+         int nLayers=0;
+        public double[] iNodes;
+        public double[][] hNodes;
+        public double[] oNodes;
+        public double[][] ihWeights;
+        public double[][][] hhWeights;
+        public double[][] hoWeights;
+        public double[][] hBiases;
+        public double[] oBiases;
+        
+
+      
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            string c = Searchcnn((Bitmap)Image.FromFile(Program.dfunduspath));
+            for (int i = 0; i < nodes.Items.Count; i++)
+            {
+                nodes.SelectedIndex = i;
+            }
+            for (int i = nodes.Items.Count-1; i>0; i--)
+            {
+                nodes.SelectedIndex = i;
+            }
+            for (int i = 0; i < nodes.Items.Count; i++)
+            {
+                nodes.SelectedIndex = i;
+            }
+            MessageBox.Show("Finished Searching");
+            if(c!="")
+            {
+                string[] results = c.Split(new[] { "***" }, StringSplitOptions.None);
+                 if(results.Count()>1)
+                { 
+                result = c;
+                D_3_Result obj = new D_3_Result(result,"1");
+                ActiveForm.Hide();
+                obj.Show();  
+                }
+                else
+                {
+                    result = c;
+                    D_3_Result obj = new D_3_Result(result, "0");
+                    ActiveForm.Hide();
+                    obj.Show();
+                }
+
+            }
+            
+        }
+
+        public string Searchcnn(Bitmap bmp)
+        {
+            int colorUnitIndex = 0;
+            int charValue = 0;
+
+          
+            string extractedText = String.Empty;
+  
+            for (int i = 0; i < bmp.Height; i++)
+            { 
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+
+                   
+                    for (int n = 0; n < 3; n++)
+                    {
+                        switch (colorUnitIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    
+                                    charValue = charValue * 2 + pixel.R % 2;
+                                } break;
+                            case 1:
+                                {
+                                    charValue = charValue * 2 + pixel.G % 2;
+                                } break;
+                            case 2:
+                                {
+                                    charValue = charValue * 2 + pixel.B % 2;
+                                } break;
+                        }
+
+                        colorUnitIndex++;
+
+                     
+                        if (colorUnitIndex % 8 == 0)
+                        {
+                         
+                            charValue = reverseBits(charValue);
+
+                         
+                            if (charValue == 0)
+                            {
+                                return extractedText;
+                            }
+
+                          
+                            char c = (char)charValue;
+
+                      
+                            extractedText += c.ToString();
+                        }
+                    }
+                }
+            }
+
+            return extractedText;
+        }
+
+        public static int reverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
