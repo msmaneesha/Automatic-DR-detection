9e59ac51d77d87cbacce5ea20b30ff885814e541
msmaneesha <69891335+msmaneesha@users.noreply.github.com>
2021-06-24 13:35:18 +0530

    Add files via upload

---
+++ T_8_Features.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace dRd
+{
+    partial class T_8_Features
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.picMRI = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMRI)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(21, 48);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.PictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(655, 479);
+            this.splitContainer1.SplitterDistance = 227;
+            this.splitContainer1.SplitterWidth = 1;
+            this.splitContainer1.TabIndex = 127;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.picMRI);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.button5);
+            this.splitContainer2.Panel2.Controls.Add(this.button2);
+            this.splitContainer2.Panel2.Controls.Add(this.comboBox1);
+            this.splitContainer2.Panel2.Controls.Add(this.label4);
+            this.splitContainer2.Panel2.Controls.Add(this.button1);
+            this.splitContainer2.Panel2.Controls.Add(this.button4);
+            this.splitContainer2.Panel2.Controls.Add(this.button3);
+            this.splitContainer2.Size = new System.Drawing.Size(227, 479);
+            this.splitContainer2.SplitterDistance = 222;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // picMRI
+            // 
+            this.picMRI.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picMRI.Location = new System.Drawing.Point(0, 0);
+            this.picMRI.Name = "picMRI";
+            this.picMRI.Size = new System.Drawing.Size(227, 222);
+            this.picMRI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMRI.TabIndex = 120;
+            this.picMRI.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(129)))), ((int)(((byte)(158)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 11.75F);
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(-6, 220);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(226, 33);
+            this.button5.TabIndex = 128;
+            this.button5.Text = "Proceed";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(129)))), ((int)(((byte)(158)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 11.75F);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(-6, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 33);
+            this.button2.TabIndex = 127;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "HEMORRHAGES",
+            "HARD EXUDATES",
+            "ANEURYSM",
+            "COTTON WOOL"});
+            this.comboBox1.Location = new System.Drawing.Point(-2, 115);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 21);
+            this.comboBox1.TabIndex = 126;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(3, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 16);
+            this.label4.TabIndex = 125;
+            this.label4.Text = "Select Features";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(129)))), ((int)(((byte)(158)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.75F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(-6, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 33);
+            this.button1.TabIndex = 124;
+            this.button1.Text = "Orginal";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(129)))), ((int)(((byte)(158)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 11.75F);
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(-6, 51);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(230, 33);
+            this.button4.TabIndex = 123;
+            this.button4.Text = "Crop";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(129)))), ((int)(((byte)(158)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 11.75F);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(-6, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(230, 33);
+            this.button3.TabIndex = 122;
+            this.button3.Text = "Make Selection";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(427, 479);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox1.TabIndex = 121;
+            this.PictureBox1.TabStop = false;
+            this.PictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.PictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            this.PictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.label7.ForeColor = System.Drawing.Color.Silver;
+            this.label7.Location = new System.Drawing.Point(669, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(172, 25);
+            this.label7.TabIndex = 142;
+            this.label7.Text = "Populating Dataset";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI Light", 31.25F);
+            this.label11.ForeColor = System.Drawing.Color.Cyan;
+            this.label11.Location = new System.Drawing.Point(463, -9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(339, 57);
+            this.label11.TabIndex = 141;
+            this.label11.Text = "Feature Extraction";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(246, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 17);
+            this.label2.TabIndex = 144;
+            this.label2.Text = "Cropped Image";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(18, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 17);
+            this.label1.TabIndex = 143;
+            this.label1.Text = "Specimen Image";
+            // 
+            // T_8_Features
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(853, 526);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "T_8_Features";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T_8_Features";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picMRI)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox picMRI;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ T_8_Features.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+
+
+namespace dRd
+{
+    public partial class T_8_Features : Form
+    {
+        private Image _originalImage;
+        private bool _selecting;
+        private Rectangle _selection;
+        private Image Img;
+        private Size OriginalImageSize;
+        private Size ModifiedImageSize;
+        int cropX;
+        int cropY;
+        int cropWidth;
+        int cropHeight;
+        int oCropX;
+        int oCropY;
+        public Pen cropPen;
+        public DashStyle cropDashStyle = DashStyle.DashDot;
+        public bool Makeselection = false;
+        public bool CreateText = false;
+        public T_8_Features()
+        {
+            InitializeComponent();
+               picMRI.Image = (Image)Program.resized;
+            PictureBox1.Image = (Image)Program.resized;
+        }
+          private void PictureBoxLocation()
+        {
+            int _x = 0;
+            int _y = 0;
+            if (splitContainer1.Panel1.Width > PictureBox1.Width)
+            {
+                _x = (splitContainer1.Panel1.Width - PictureBox1.Width) / 2;
+            }
+            if (splitContainer1.Panel1.Height > PictureBox1.Height)
+            {
+                _y = (splitContainer1.Panel1.Height - PictureBox1.Height) / 2;
+            }
+            PictureBox1.Location = new Point(_x, _y);
+        }
+
+          private void button4_Click(object sender, EventArgs e)
+          {
+              try
+              {
+                  if (cropWidth < 1)
+                  {
+                      return;
+                  }
+                  Rectangle rect = new Rectangle(cropX, cropY, cropWidth, cropHeight);
+                  //First we define a rectangle with the help of already calculated points
+                  Bitmap OriginalImage = new Bitmap(PictureBox1.Image, PictureBox1.Width, PictureBox1.Height);
+                  //Original image
+                  Bitmap _img = new Bitmap(cropWidth, cropHeight);
+                  // for cropinf image
+                  Graphics g = Graphics.FromImage(_img);
+                  // create graphics
+                  g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                  g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                  g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                  //set image attributes
+                  g.DrawImage(OriginalImage, 0, 0, rect, GraphicsUnit.Pixel);
+
+                  PictureBox1.Image = _img;
+                  PictureBox1.Width = _img.Width;
+                  PictureBox1.Height = _img.Height;
+                  PictureBoxLocation();
+
+              }
+              catch (Exception ex)
+              {
+              }
+          }
+
+          private void button3_Click(object sender, EventArgs e)
+          {
+              Makeselection = true;
+          }
+
+          private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+          {
+              Cursor = Cursors.Default;
+              if (Makeselection)
+              {
+
+                  try
+                  {
+                      if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                      {
+                          Cursor = Cursors.Cross;
+                          cropX = e.X;
+                          cropY = e.Y;
+
+                          cropPen = new Pen(Color.Yellow, 1);
+                          cropPen.DashStyle = DashStyle.DashDotDot;
+
+
+                      }
+                      PictureBox1.Refresh();
+                  }
+                  catch (Exception ex)
+                  {
+                  }
+              }
+          }
+
+          private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+          {
+
+              Cursor = Cursors.Default;
+              if (Makeselection)
+              {
+
+                  try
+                  {
+                      if (PictureBox1.Image == null)
+                          return;
+
+
+                      if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                      {
+                          PictureBox1.Refresh();
+                          cropWidth = e.X - cropX;
+                          cropHeight = e.Y - cropY;
+                          PictureBox1.CreateGraphics().DrawRectangle(cropPen, cropX, cropY, cropWidth, cropHeight);
+                      }
+
+
+
+                  }
+                  catch (Exception ex)
+                  {
+
+                  }
+              }
+          }
+
+          private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+          {
+              if (Makeselection)
+              {
+                  Cursor = Cursors.Default;
+
+              }
+          }
+
+          private void button2_Click(object sender, EventArgs e)
+          {
+
+
+              int width = Convert.ToInt32(PictureBox1.Image.Width);
+              int height = Convert.ToInt32(PictureBox1.Image.Height);
+              Bitmap bmp = new Bitmap(width, height);
+              DirectoryInfo info = new DirectoryInfo(Application.StartupPath + "\\db_trainimages");
+              string[] array1 = Directory.GetFiles(Application.StartupPath + "\\db_trainimages");
+              int count = 0;
+              count = array1.Count();
+              if (comboBox1.Text == "HEMORRHAGES")
+              {
+                  Program.hemorrages = "true";
+                  Program.he = (Bitmap)PictureBox1.Image;
+
+              }
+              else if (comboBox1.Text == "HARD EXUDATES")
+              {
+                  Program.exudates = "true";
+                  Program.ex = (Bitmap)PictureBox1.Image;
+              }
+              else if (comboBox1.Text == "ANEURYSM")
+              {
+                  Program.aneurysm = "true";
+                  Program.an = (Bitmap)PictureBox1.Image;
+              }
+              else if (comboBox1.Text == "COTTON WOOL")
+              {
+                  Program.cottonwool = "true";
+                  Program.co = (Bitmap)PictureBox1.Image;
+              }
+              PictureBox1.Image.Save(Application.StartupPath + "\\db_trainimages\\" + comboBox1.Text +"_"+count.ToString()+ ".jpeg", ImageFormat.Png);
+              MessageBox.Show(comboBox1.Text + " Image saved...");
+              PictureBox1.Image = (Image)Program.resized;
+              comboBox1.Text = "";
+          }
+
+          private void button1_Click(object sender, EventArgs e)
+          {
+              PictureBox1.Image = (Image)Program.resized;
+          }
+
+          private void button5_Click(object sender, EventArgs e)
+          {
+              T_9_Results obj = new T_9_Results();
+              ActiveForm.Hide();
+              obj.Show();
+          }
+
+        }
+   }
+
